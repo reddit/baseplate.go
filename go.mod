6d6ed0976725b82be0d11b754bbd817250077bfa
Liliya Belaus <liliya.belaus@reddit.com>
2019-11-24 19:55:53 -0500

    Set up Zipkin Tracer

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.13
 
 require (
 	github.com/apache/thrift v0.13.0
-	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/opentracing/opentracing-go v1.1.0
+	github.com/openzipkin-contrib/zipkin-go-opentracing v0.4.4
+	github.com/openzipkin/zipkin-go v0.2.1
 	golang.org/x/sys v0.0.0-20191008105621-543471e840be // indirect
 	gopkg.in/fsnotify.v1 v1.4.7
 )
---
+++ tracing/tracer.go
@@ -0,0 +1,93 @@
+package tracing
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/opentracing/opentracing-go"
+	zipkinot "github.com/openzipkin-contrib/zipkin-go-opentracing"
+	zipkin "github.com/openzipkin/zipkin-go"
+	"github.com/openzipkin/zipkin-go/reporter"
+	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
+)
+
+var zipkinReporter reporter.Reporter
+
+// Tracer is exported to support start tracible server etc
+var Tracer = opentracing.GlobalTracer()
+
+func InitTracer(serviceName, addr string, sampleRate float64) {
+	if addr == "" {
+		// TODO: log info level
+		return
+	}
+
+	zipkinReporter := zipkinhttp.NewReporter(addr)
+
+	endpoint, err := zipkin.NewEndpoint(serviceName, "0.0.0.0:0")
+	if err != nil {
+		log.Fatalf("unable to create local endpoint: %+v\n", err)
+	}
+
+	sampler, err := zipkin.NewCountingSampler(sampleRate)
+	if err != nil {
+		log.Fatalf("unable to create sampler: %+v\n", err)
+	}
+
+	nativeTracer, err := zipkin.NewTracer(
+		zipkinReporter,
+		zipkin.WithSampler(sampler),
+		zipkin.WithLocalEndpoint(endpoint),
+	)
+
+	tracer := zipkinot.Wrap(nativeTracer)
+	opentracing.SetGlobalTracer(tracer)
+
+	// TODO: log info level
+}
+
+// CloseZipkinReporter close the reporter started in init trace stage
+func CloseZipkinReporter() {
+	zipkinReporter.Close()
+}
+
+// StartSpanFromParent creates a sub span from parent
+func StartSpanFromParent(optName string, parent opentracing.Span) opentracing.Span {
+	var child opentracing.Span
+
+	if parent != nil {
+		child = Tracer.StartSpan(
+			optName,
+			opentracing.ChildOf(parent.Context()),
+		)
+	}
+
+	return child
+}
+
+// EndSpan finish the span with option messages
+func EndSpan(span opentracing.Span, opts ...interface{}) {
+	if span == nil {
+		// TODO: log debug level - missing span to close
+		return
+	}
+
+	if len(opts) > 0 {
+		if len(opts)%2 != 0 {
+			opts = append(opts, "Missing Value")
+		}
+
+		values := make([]opentracing.LogRecord, len(opts)/2)
+		for i := 0; i < len(opts); i += 2 {
+			ld := &opentracing.LogData{
+				Event:   fmt.Sprintf("%v", opts[i]),
+				Payload: opts[i+1],
+			}
+			values = append(values, ld.ToLogRecord())
+		}
+
+		span.FinishWithOptions(opentracing.FinishOptions{LogRecords: values})
+	} else {
+		span.Finish()
+	}
+}
