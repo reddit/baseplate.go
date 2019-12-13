ce3a321efb496dfe2bef3d4634ce755341fa6526
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2019-12-13 09:50:11 -0800

    Add metricsbp package
    It provides a statsd based metrics implementation (based on go-kit), and
    also related helper functions.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/reddit/baseplate.go
 go 1.13
 
 require (
+	github.com/VividCortex/gohistogram v1.0.0 // indirect
 	github.com/apache/thrift v0.13.0
 	github.com/certifi/gocertifi v0.0.0-20191021191039-0944d244cd40 // indirect
 	github.com/getsentry/raven-go v0.2.0
---
+++ metricsbp/example_nil_check_test.go
@@ -0,0 +1,64 @@
+package metricsbp_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/reddit/baseplate.go/metricsbp"
+
+	"github.com/go-kit/kit/metrics"
+)
+
+type SubMetrics struct {
+	MyHistogram metrics.Histogram
+	MyGauge     metrics.Gauge
+}
+
+type PreCreatedMetrics struct {
+	MyCounter    metrics.Counter
+	MySubMetrics SubMetrics
+}
+
+// This example demonstrate how to use CheckNilFields in your microservice code
+// to pre-create frequently used metrics.
+func ExampleCheckNilFields() {
+	// In reality these should come from flag or other configurations.
+	const (
+		prefix     = "myservice"
+		statsdAddr = "localhost:1234"
+		sampleRate = 1
+	)
+
+	st := metricsbp.NewStatsd(
+		context.Background(),
+		metricsbp.StatsdConfig{
+			Prefix:            prefix,
+			Address:           statsdAddr,
+			DefaultSampleRate: sampleRate,
+		},
+	)
+	defer st.StopReporting()
+
+	// Initialize metrics
+	m := PreCreatedMetrics{
+		MyCounter: st.Counter("my.counter"),
+		MySubMetrics: SubMetrics{
+			MyHistogram: st.Histogram("my.histogram"),
+			// Forgot to initialize MyGauge here
+		},
+	}
+	missingFields := metricsbp.CheckNilFields(m)
+	if len(missingFields) > 0 {
+		panic(fmt.Sprintf("Uninitialized metrics: %v", missingFields))
+	}
+
+	// Other initializations.
+
+	// Replace with your actual service starter
+	startService := func(m PreCreatedMetrics /* and other args */) {}
+
+	startService(
+		m,
+		// other args
+	)
+}
