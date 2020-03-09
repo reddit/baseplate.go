09c47d965d5952b546f7f6b0a26e6608f9a67d47
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2020-03-09 13:24:15 -0700

    Better way to run bazel tests in CI
    We have --test_output=errors so there's no need to print out test logs
    in a shell script.

--- scripts/ci-run-bazel-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-bazel --bazelrc=.bazelrc.ci test //...:all
-
-if [ $? -ne 0 ]; then
-  # Test failed, print out all test logs then exit with non-zero code.
-  find bazel-testlogs/ -name "test.xml" -print -exec grep -C 1 Failed {} \;
-  exit 1
-fi
