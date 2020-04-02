ed61dd1a6e2f71e8e5ee616664d9805ce276670a
Andrew Boyle <andrew.boyle@reddit.com>
2020-04-02 12:13:07 -0700

    Add thriftbp.NewThriftServer
    This is a conveinence function provided to remove some of the
    boilerplate of instantiating a Baseplate Thrift service.
    
    Note that it uses the THeader protocol, if your service needs to
    use a different protocol, it is not appropriate for that use
    case.

---
+++ thriftbp/example_server_test.go
@@ -0,0 +1,38 @@
+package thriftbp_test
+
+import (
+	"time"
+
+	"github.com/reddit/baseplate.go/edgecontext"
+	"github.com/reddit/baseplate.go/internal/gen-go/reddit/baseplate"
+	"github.com/reddit/baseplate.go/log"
+	"github.com/reddit/baseplate.go/thriftbp"
+	"github.com/reddit/baseplate.go/tracing"
+)
+
+// This example demonstrates how to use thriftbp.NewThriftServer.
+func ExampleNewThriftServer() {
+	// variables should be initialized properly in production
+	var (
+		ecImpl  *edgecontext.Impl
+		handler baseplate.BaseplateService
+		logger  log.Wrapper
+	)
+
+	processor := baseplate.NewBaseplateServiceProcessor(handler)
+	server, err := thriftbp.NewThriftServer(
+		thriftbp.ServerConfig{
+			Addr:    "localhost:8080",
+			Timeout: time.Second,
+			Logger:  logger,
+		},
+		processor,
+		edgecontext.InjectThriftEdgeContext(ecImpl, logger),
+		tracing.InjectThriftServerSpan,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(server.Serve())
+}
