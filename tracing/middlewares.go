f99981db460360fb04555e90d3ee72bf5fa269ad
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2020-04-03 20:27:37 -0700

    tracing: Fix middleware function doc comments

--- tracing/middlewares.go
+++ tracing/middlewares.go
@@ -9,8 +9,8 @@ import (
 	"github.com/reddit/baseplate.go/thriftbp"
 )
 
-// InjectHTTPServerSpan returns a go-kit endpoint.Middleware that injects a server
-// span into the `next` context.
+// InjectHTTPServerSpan returns a go-kit endpoint.Middleware that injects a
+// server span into the `next` context.
 //
 // Starts the server span before calling the `next` endpoint and stops the span
 // after the endpoint finishes.
@@ -18,9 +18,10 @@ import (
 // response implements ErrorResponse, the error returned by Err() will not be
 // passed to span.Stop.
 //
-// Note, this function depends on the edge context headers already being set on
-// the context object.  This can be done by adding httpbp.PopulateRequestContext
-// as a ServerBefore option when setting up the request handler for an endpoint.
+// Note, this function depends on the tracing headers already being set on the
+// context object.
+// This can be done by adding httpbp.PopulateRequestContext as a ServerBefore
+// option when setting up the request handler for an endpoint.
 func InjectHTTPServerSpan(name string) endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
@@ -44,9 +45,9 @@ func InjectHTTPServerSpan(name string) endpoint.Middleware {
 // the span after it finishes.
 // If the function returns an error, that will be passed to span.Stop.
 //
-// Note, this depends on the edge context headers already being set on the
-// context object.  These should be automatically injected by your
-// thrift.TSimpleServer.
+// Note, the span will be created according to tracing related headers already
+// being set on the context object.
+// These should be automatically injected by your thrift.TSimpleServer.
 func InjectThriftServerSpan(name string, next thrift.TProcessorFunction) thrift.TProcessorFunction {
 	return thriftbp.WrappedTProcessorFunc{
 		Wrapped: func(ctx context.Context, seqId int32, in, out thrift.TProtocol) (success bool, err thrift.TException) {
