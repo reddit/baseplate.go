4dcdd6b87437e5887753eedcc6e6af1974637672
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2020-04-03 20:01:15 -0700

    thriftbp: Change the return type of Merge to BaseplateProcessor

--- thriftbp/merger.go
+++ thriftbp/merger.go
@@ -1,17 +1,15 @@
 package thriftbp
 
-import (
-	"github.com/apache/thrift/lib/go/thrift"
-)
-
-// Merge merges together the specified processors.
+// Merge merges together multiple processors into the first one.
 //
-// It's useful if the server need to support two separated thrift files.
+// It's useful when the server needs to support more than one separated thrift
+// file.
 //
 // It's kind of like Apache Thrift's TMultiplexedProcessor, the key difference
 // is that TMultiplexedProcessor requires the client to also use
-// TMultiplexedProtocol, while here the client didn't need any special handling.
-func Merge(processors ...BaseplateProcessor) thrift.TProcessor {
+// TMultiplexedProtocol, while here the client doesn't need any special
+// handling.
+func Merge(processors ...BaseplateProcessor) BaseplateProcessor {
 	firstProcessor := processors[0]
 	for i := 1; i < len(processors); i++ {
 		processor := processors[i]
