5f7c91481c9bb117da13772be302590a9de3fe43
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2020-04-27 13:53:51 -0700

    log: Add cheatcode to make sure KitLogger works
    Also remove an irrelevant test.

--- log/log_test.go
+++ log/log_test.go
@@ -1,18 +1,8 @@
 package log
 
 import (
-	"os"
 	"testing"
-
-	"github.com/go-kit/kit/log"
 )
-
-func TestKitLogger(t *testing.T) {
-	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
-	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
-
-	logger.Log("msg", "printing flag values")
-}
 
 func TestZapLogger(t *testing.T) {
 	InitLogger(DebugLevel)
