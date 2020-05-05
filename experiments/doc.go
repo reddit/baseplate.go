dcf47c99c328ae18eadf74f0f375bc52422fe9af
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2020-05-05 15:49:26 -0700

    experiments: Add starting point in package doc
    This package contains a lot of types and it could be confusing for
    users to figure out where to start.

--- experiments/doc.go
+++ experiments/doc.go
@@ -1,3 +1,5 @@
 // Package experiments provides implementation of experiments config parsing
 // and other experiments related features according to baseplate spec.
+//
+// For a starting point, see Experiments type and NewExperiments function,
 package experiments
