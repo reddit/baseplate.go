16a9096a23f17f84f05272b2bd38ce5c5cba8921
Andrew Boyle <andrew.boyle@reddit.com>
2020-05-16 14:09:47 -0700

    Add batchcloser package
    Package batchcloser provides an object "BatchCloser" that collects multiple
    io.Closers and closes them all when Closers.Close is called.
    
    It also provides helper structs for wrapping close/cancel functions in
    a struct that implements io.Closer.

---
+++ batchcloser/closers.go
@@ -0,0 +1,110 @@
+package batchcloser
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/reddit/baseplate.go/batcherror"
+)
+
+// CloseError is used to wrap the errors returned by the inner closers within a
+// BatchCloser.
+//
+// It can be used to inspect both the error that was returned and the io.Closer
+// that caused it.
+type CloseError struct {
+	Cause  error
+	Closer io.Closer
+}
+
+// Error implements the interface for error.
+func (err CloseError) Error() string {
+	return fmt.Sprintf("batchcloser: error closing closer %#v : %s", err.Closer, err.Cause.Error())
+}
+
+// Unwrap implements helper interface for errors.Is.
+func (err CloseError) Unwrap() error {
+	return err.Cause
+}
+
+// As implements helper interface for errors.As.
+func (err CloseError) As(v interface{}) bool {
+	if target, ok := v.(*CloseError); ok {
+		*target = err
+		return true
+	}
+	if target, ok := v.(**CloseError); ok {
+		*target = &err
+		return true
+	}
+	if errors.As(err.Cause, v) {
+		return true
+	}
+	return false
+}
+
+type simpleCloser struct {
+	close func() error
+}
+
+func (c simpleCloser) Close() error {
+	return c.close()
+}
+
+// Wrap can be used wrap close functions in an io.Closer.
+func Wrap(close func() error) io.Closer {
+	return simpleCloser{close: close}
+}
+
+// WrapCancel can be used to wrap a context.CancelFunc in an io.Closer.
+func WrapCancel(cancel context.CancelFunc) io.Closer {
+	return Wrap(func() error {
+		cancel()
+		return nil
+	})
+}
+
+// New returns a pointer to a new BatchCloser initialized with the given closers.
+func New(closers ...io.Closer) *BatchCloser {
+	bc := &BatchCloser{}
+	bc.Add(closers...)
+	return bc
+}
+
+// BatchCloser is a collection of io.Closer objects that are all closed when
+// BatchCloser.Close is called.
+type BatchCloser struct {
+	closers []io.Closer
+}
+
+// Close implements io.Closer and closes all of it's internal io.Closer objects,
+// batching any errors into a batcherror.BatchError.
+func (bc *BatchCloser) Close() error {
+	var errs batcherror.BatchError
+	for _, closer := range bc.closers {
+		if err := closer.Close(); err != nil {
+			errs.Add(CloseError{
+				Cause:  err,
+				Closer: closer,
+			})
+		}
+	}
+	return errs.Compile()
+}
+
+// Add adds the given io.Closer objects to the BatchCloser.
+//
+// This is not safe to be called concurrently.
+func (bc *BatchCloser) Add(closers ...io.Closer) {
+	bc.closers = append(bc.closers, closers...)
+}
+
+var (
+	_ error     = CloseError{}
+	_ error     = (*CloseError)(nil)
+	_ io.Closer = simpleCloser{}
+	_ io.Closer = (*simpleCloser)(nil)
+	_ io.Closer = (*BatchCloser)(nil)
+)
---
+++ batchcloser/closers_test.go
@@ -0,0 +1,268 @@
+package batchcloser_test
+
+import (
+	"context"
+	"errors"
+	"io"
+	"testing"
+
+	"github.com/reddit/baseplate.go/batchcloser"
+)
+
+type closeRecorder struct {
+	closed bool
+	err    error
+}
+
+func (c *closeRecorder) Close() error {
+	c.closed = true
+	return c.err
+}
+
+func TestCloseErrorAs(t *testing.T) {
+	t.Parallel()
+
+	cause := errors.New("test error")
+	closer := &closeRecorder{}
+
+	t.Run(
+		"as-pointer",
+		func(t *testing.T) {
+			var err error
+			var closeErr batchcloser.CloseError
+
+			err = &batchcloser.CloseError{
+				Cause:  cause,
+				Closer: closer,
+			}
+
+			if errors.As(err, &closeErr) {
+				if closeErr.Closer != closer {
+					t.Errorf("closer mismatch, expected %#v, got %#v", closer, closeErr.Closer)
+				}
+				if !errors.Is(closeErr, cause) {
+					t.Errorf("close error %#v did not wrap the expected error %#v", closeErr, cause)
+				}
+			} else {
+				t.Fatalf("%#v could not be converted to CloseError", err)
+			}
+		},
+	)
+
+	t.Run(
+		"as-struct",
+		func(t *testing.T) {
+			var err error
+			var closeErr batchcloser.CloseError
+
+			err = batchcloser.CloseError{
+				Cause:  cause,
+				Closer: closer,
+			}
+
+			if errors.As(err, &closeErr) {
+				if closeErr.Closer != closer {
+					t.Errorf("closer mismatch, expected %#v, got %#v", closer, closeErr.Closer)
+				}
+				if !errors.Is(closeErr, cause) {
+					t.Errorf("close error %#v did not wrap the expected error %#v", closeErr, cause)
+				}
+			} else {
+				t.Fatalf("%#v could not be converted to CloseError", err)
+			}
+		},
+	)
+
+	t.Run(
+		"unwrap",
+		func(t *testing.T) {
+			err := batchcloser.CloseError{
+				Cause:  cause,
+				Closer: closer,
+			}
+			if !errors.Is(err, cause) {
+				t.Errorf("close error %#v did not wrap the expected error %#v", err, cause)
+			}
+		},
+	)
+}
+
+func TestWrap(t *testing.T) {
+	t.Parallel()
+
+	t.Run(
+		"no-error",
+		func(t *testing.T) {
+			recorder := &closeRecorder{}
+
+			closer := batchcloser.Wrap(recorder.Close)
+			if err := closer.Close(); err != nil {
+				t.Fatal(err)
+			}
+
+			if !recorder.closed {
+				t.Error("recorder was not closed")
+			}
+		},
+	)
+
+	t.Run(
+		"error",
+		func(t *testing.T) {
+			recorder := &closeRecorder{
+				err: errors.New("test error"),
+			}
+
+			closer := batchcloser.Wrap(recorder.Close)
+			if err := closer.Close(); !errors.Is(err, recorder.err) {
+				t.Errorf("error mismatch, expected %v, got %v", recorder.err, err)
+			}
+
+			if !recorder.closed {
+				t.Error("recorder was not closed")
+			}
+		},
+	)
+}
+
+func TestWrapCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	closer := batchcloser.WrapCancel(cancel)
+
+	if err := closer.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := ctx.Err(); !errors.Is(err, context.Canceled) {
+		t.Errorf("unexpected error type %v, expected %v", err, context.Canceled)
+	}
+}
+
+func TestBatchCloser(t *testing.T) {
+	t.Parallel()
+
+	t.Run(
+		"base",
+		func(t *testing.T) {
+			first := &closeRecorder{}
+			second := &closeRecorder{}
+			bc := batchcloser.New(first, second)
+			if err := bc.Close(); err != nil {
+				t.Fatal(err)
+			}
+			if !first.closed {
+				t.Errorf("first closer was not closed.")
+			}
+			if !second.closed {
+				t.Errorf("second closer was not closed.")
+			}
+		},
+	)
+
+	t.Run(
+		"mixed-errors",
+		func(t *testing.T) {
+			first := &closeRecorder{}
+			second := &closeRecorder{err: errors.New("test error")}
+			bc := batchcloser.New(first, second)
+			err := bc.Close()
+			if !errors.Is(err, second.err) {
+				t.Errorf("error from second closer was not returned.")
+			}
+			if !first.closed {
+				t.Errorf("first closer was not closed.")
+			}
+			if !second.closed {
+				t.Errorf("second closer was not closed.")
+			}
+		},
+	)
+
+	cases := []struct {
+		name       string
+		numClosers int
+	}{
+		{
+			name: "zero",
+		},
+		{
+			name:       "one",
+			numClosers: 1,
+		},
+		{
+			name:       "two",
+			numClosers: 2,
+		},
+	}
+
+	for _, _c := range cases {
+		c := _c
+		t.Run(
+			c.name,
+			func(t *testing.T) {
+				// We have to use a list of []io.Closers to use them with the `...`
+				// syntax when passing to Add.
+				initial := make([]io.Closer, 0, c.numClosers)
+				for i := 0; i < c.numClosers; i++ {
+					initial = append(initial, &closeRecorder{})
+				}
+				bc := batchcloser.New(initial...)
+				recorder := &closeRecorder{}
+				bc.Add(recorder)
+				if err := bc.Close(); err != nil {
+					t.Fatal(err)
+				}
+				for i, c := range initial {
+					if closer, ok := c.(*closeRecorder); !ok {
+						t.Errorf("closer %d is not a *closeRecorder", i)
+					} else if !closer.closed {
+						t.Errorf("closer %d was not closed", i)
+					}
+				}
+				if !recorder.closed {
+					t.Errorf("additional closer was not closed.")
+				}
+			},
+		)
+
+		t.Run(
+			c.name+"/errors",
+			func(t *testing.T) {
+				// We have to use a list of []io.Closers to use them with the `...`
+				// syntax when passing to Add.
+				initial := make([]io.Closer, 0, c.numClosers)
+				for i := 0; i < c.numClosers; i++ {
+					initial = append(initial, &closeRecorder{
+						err: errors.New("test error"),
+					})
+				}
+				bc := batchcloser.New(initial...)
+				recorder := &closeRecorder{
+					err: errors.New("test error"),
+				}
+				bc.Add(recorder)
+
+				err := bc.Close()
+				if err == nil {
+					t.Fatal("expected an error, got nil")
+				}
+
+				for i, c := range initial {
+					if closer, ok := c.(*closeRecorder); !ok {
+						t.Errorf("closer %d is not a *closeRecorder", i)
+					} else {
+						if !closer.closed {
+							t.Errorf("closer %d was not closed", i)
+						}
+						if !errors.Is(err, closer.err) {
+							t.Errorf("closer %d's error was not represented in the returned error", i)
+						}
+					}
+				}
+				if !recorder.closed {
+					t.Errorf("additional closer was not closed.")
+				}
+			},
+		)
+	}
+}
