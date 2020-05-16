1977d69f8f15d4d39b397752458cc207e7365717
Vladimir Kuptsov <vkuptcov@gmail.com>
2020-05-16 22:14:35 +0200

    Ignore redis nil errors (#190)
    Baseplate adds a pipeline hook to batch Redis pipeline errors.
    The problem is that Redis returns redis.Nil error in some cases, e.g. key absence.
    These errors should be ignored.
    
    Also, the fix doesn't return errors from the hook in order not to change the default go-redis client error.
    
    Co-authored-by: Vladimir Kuptsov <v.vladimir.kuptsov@reddit.com>

--- redisbp/hooks.go
+++ redisbp/hooks.go
@@ -2,6 +2,7 @@ package redisbp
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/go-redis/redis/v7"
@@ -29,7 +30,12 @@ func (h SpanHook) BeforeProcess(ctx context.Context, cmd redis.Cmder) (context.C
 // time the Redis command took to complete, and a metric indicating whether the
 // command was a "success" or "fail"
 func (h SpanHook) AfterProcess(ctx context.Context, cmd redis.Cmder) error {
-	return h.endChildSpan(ctx, cmd.Err())
+	h.endChildSpan(ctx, cmd.Err())
+	// NOTE: returning non-nil error from the hook changes the error the caller gets.
+	// for this particular case if we return cmd.Err(), it will not change the client error,
+	// but anyway it's not necessary
+	// see: https://github.com/go-redis/redis/blob/v7.2.0/redis.go#L60
+	return nil
 }
 
 // BeforeProcessPipeline starts a client span before processing a Redis pipeline
@@ -44,9 +50,14 @@ func (h SpanHook) BeforeProcessPipeline(ctx context.Context, cmds []redis.Cmder)
 func (h SpanHook) AfterProcessPipeline(ctx context.Context, cmds []redis.Cmder) error {
 	var errs batcherror.BatchError
 	for _, cmd := range cmds {
-		errs.Add(cmd.Err())
+		if !errors.Is(cmd.Err(), redis.Nil) {
+			errs.Add(cmd.Err())
+		}
 	}
-	return h.endChildSpan(ctx, errs.Compile())
+	h.endChildSpan(ctx, errs.Compile())
+	// NOTE: returning non-nil error from the hook changes the error the caller gets, and that's something we want to avoid.
+	// see: https://github.com/go-redis/redis/blob/v7.2.0/redis.go#L101
+	return nil
 }
 
 func (h SpanHook) startChildSpan(ctx context.Context, cmdName string) context.Context {
@@ -59,12 +70,11 @@ func (h SpanHook) startChildSpan(ctx context.Context, cmdName string) context.Co
 	return ctx
 }
 
-func (h SpanHook) endChildSpan(ctx context.Context, err error) error {
+func (h SpanHook) endChildSpan(ctx context.Context, err error) {
 	if span := opentracing.SpanFromContext(ctx); span != nil {
 		span.FinishWithOptions(tracing.FinishOptions{
 			Ctx: ctx,
 			Err: err,
 		}.Convert())
 	}
-	return err
 }
