1e272f68ac3a403171668d9519aff31556e3a131
Andrew Boyle <andrew.boyle@reddit.com>
2020-05-19 10:48:54 -0700

    fixup: use NewTestSecrets

--- thriftbp/thrifttest/server_test.go
+++ thriftbp/thrifttest/server_test.go
@@ -3,28 +3,12 @@ package thrifttest_test
 import (
 	"context"
 	"errors"
-	"io"
-	"io/ioutil"
-	"os"
 	"testing"
 
-	"github.com/reddit/baseplate.go/batcherror"
 	baseplatethrift "github.com/reddit/baseplate.go/internal/gen-go/reddit/baseplate"
 	"github.com/reddit/baseplate.go/secrets"
 	"github.com/reddit/baseplate.go/thriftbp/thrifttest"
 )
-
-// pubkey copied from https://github.com/reddit/baseplate.py/blob/db9c1d7cddb1cb242546349e821cad0b0cbd6fce/tests/__init__.py#L12
-const secretStore = `
-{
-	"secrets": {
-		"secret/authentication/public-key": {
-			"type": "versioned",
-			"current": "foobar",
-			"previous": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtzMnDEQPd75QZByogNlB\nNY2auyr4sy8UNTDARs79Edq/Jw5tb7ub412mOB61mVrcuFZW6xfmCRt0ILgoaT66\nTp1RpuEfghD+e7bYZ+Q2pckC1ZaVPIVVf/ZcCZ0tKQHoD8EpyyFINKjCh516VrCx\nKuOm2fALPB/xDwDBEdeVJlh5/3HHP2V35scdvDRkvr2qkcvhzoy0+7wUWFRZ2n6H\nTFrxMHQoHg0tutAJEkjsMw9xfN7V07c952SHNRZvu80V5EEpnKw/iYKXUjCmoXm8\ntpJv5kXH6XPgfvOirSbTfuo+0VGqVIx9gcomzJ0I5WfGTD22dAxDiRT7q7KZnNgt\nTwIDAQAB\n-----END PUBLIC KEY-----"
-		}
-	}
-}`
 
 type BaseplateService struct {
 	Fail bool
@@ -35,64 +19,22 @@ func (srv BaseplateService) IsHealthy(ctx context.Context) (r bool, err error) {
 	return !srv.Fail, srv.Err
 }
 
-type Closer struct {
-	Closers []func() error
-}
-
-func (c *Closer) Add(closer func() error) {
-	c.Closers = append(c.Closers, closer)
-}
-
-func (c *Closer) Close() error {
-	var errs batcherror.BatchError
-	for _, closer := range c.Closers {
-		if err := closer(); err != nil {
-			errs.Add(err)
-		}
-	}
-	return errs.Compile()
-}
-
-func newSecrets(t testing.TB) (*secrets.Store, io.Closer) {
+func newSecrets(t testing.TB) *secrets.Store {
 	t.Helper()
 
-	closer := &Closer{}
-	dir, err := ioutil.TempDir("", "thifttest-server-test-")
+	store, _, err := secrets.NewTestSecrets(
+		context.Background(),
+		make(map[string]secrets.GenericSecret),
+	)
 	if err != nil {
 		t.Fatal(err)
 	}
-	closer.Add(func() error {
-		return os.RemoveAll(dir)
-	})
-
-	tmpFile, err := ioutil.TempFile(dir, "secrets.json")
-	if err != nil {
-		closer.Close()
-		t.Fatal(err)
-	}
-
-	tmpPath := tmpFile.Name()
-	if _, err := tmpFile.Write([]byte(secretStore)); err != nil {
-		closer.Close()
-		t.Fatal(err)
-	}
-	if err := tmpFile.Close(); err != nil {
-		closer.Close()
-		t.Fatal(err)
-	}
-
-	store, err := secrets.NewStore(context.Background(), tmpPath, nil)
-	if err != nil {
-		closer.Close()
-		t.Fatal(err)
-	}
-	closer.Add(store.Close)
-	return store, closer
+	return store
 }
 
 func TestNewBaseplateServer(t *testing.T) {
-	store, closer := newSecrets(t)
-	defer closer.Close()
+	store := newSecrets(t)
+	defer store.Close()
 
 	testErr := errors.New("test")
 
