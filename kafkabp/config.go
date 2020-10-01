a808c5c0a9d3661ae806cb746b8360ae9a562c7b
Sergei Uversky <suversky@users.noreply.github.com>
2020-10-01 13:33:47 -0600

    Add YAML tags

--- kafkabp/config.go
+++ kafkabp/config.go
@@ -5,19 +5,19 @@ import "github.com/Shopify/sarama"
 // ConsumerConfig is the configuration struct passed to NewConsumer.
 type ConsumerConfig struct {
 	// Required.
-	Brokers []string
-	Topic   string
+	Brokers []string `yaml:"brokers"`
+	Topic   string   `yaml:"topic"`
 
 	// Optional. Defaults to DefaultSaramaConfig.
 	SaramaConfig *sarama.Config
 
 	// Optional. If set, overrides the client ID set in SaramaConfig.
-	ClientID string
+	ClientID string `yaml:"clientID"`
 
 	// Optional. Defaults to OffsetOldest.
-	Offset KafkaOffset
+	Offset KafkaOffset `yaml:"kafkaOffset"`
 
 	// Optional. Defaults to true. Set to false to prevent a server span from
 	// being created for every Kafka message consumed.
-	Tracing *bool
+	Tracing *bool `yaml:"tracing"`
 }
