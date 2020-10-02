dbf1a7f491632c25adf8c38046125767ec4a3c96
Sergei Uversky <suversky@users.noreply.github.com>
2020-10-02 17:50:12 -0600

    [WIP] Rework Sarama configuration in ConsumerConfig

--- kafkabp/config.go
+++ kafkabp/config.go
@@ -9,11 +9,23 @@ type ConsumerConfig struct {
 	Topic   string   `yaml:"topic"`
 
 	// Optional. Defaults to DefaultSaramaConfig.
-	SaramaConfig *sarama.Config
+	SaramaConfig SaramaConsumerConfig `yaml:"sarama"`
 
-	// Optional. If set, overrides the client ID set in SaramaConfig.
-	ClientID string `yaml:"clientID"`
-
-	// Optional. Defaults to OffsetOldest.
+	// Optional. Defaults to OffsetOldest. Valid values are specified as
+	// constants in sarama_wrapper.go.
 	Offset int64 `yaml:"kafkaOffset"`
+}
+
+// NewSaramaConfig instantiates sarama.Config with sane defaults from
+// sarama.NewConfig(), overwritten by any values parsed from cfg.SaramaConfig.
+func (cfg ConsumerConfig) NewSaramaConfig() (*sarama.Config, error) {
+	c := sarama.NewConfig()
+}
+
+// SaramaConsumerConfig specifies Sarama settings relevant to a Kafka consumer.
+// It uses
+type SaramaConsumerConfig struct {
+	// Optional. A user-provided string sent with every request to the brokers
+	// for logging, debugging, and auditing purposes. Defaults to "sarama".
+	ClientID string `yaml:"clientID"`
 }
