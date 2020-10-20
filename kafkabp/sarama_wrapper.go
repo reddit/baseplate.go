fbfa20b629a08303cc2f25c2a1f00178cdf044a7
Sergei Uversky <suversky@users.noreply.github.com>
2020-10-20 16:26:13 -0600

    [WIP] Rework errors and remove unused function

--- kafkabp/sarama_wrapper.go
+++ kafkabp/sarama_wrapper.go
@@ -17,17 +17,18 @@ const (
 )
 
 var (
-	// ErrClientIDEmpty is thrown when the client ID is empty.
-	ErrClientIDEmpty = errors.New("kafkabp: ClientID is empty")
-
 	// ErrBrokersEmpty is thrown when the slice of brokers is empty.
 	ErrBrokersEmpty = errors.New("kafkabp: Brokers are empty")
 
 	// ErrTopicEmpty is thrown when the topic is empty.
 	ErrTopicEmpty = errors.New("kafkabp: Topic is empty")
-)
 
-// DefaultSaramaConfig returns the sane default Sarama configuration.
-func DefaultSaramaConfig() *sarama.Config {
-	return sarama.NewConfig()
-}
+	// ErrSaramaConfigEmpty is thrown when the Sarama configuration provided is empty.
+	ErrSaramaConfigEmpty = errors.New("kafkabp: Sarama configuration is empty")
+
+	// ErrLocalAddrEmpty is thrown when a custom Sarama configuration is provided without a local address specified.
+	ErrLocalAddrEmpty = errors.New("kafkabp: LocalAddr is empty")
+
+	// ErrOffsetInvalid is thrown when an invalid offset is specified.
+	ErrOffsetInvalid = errors.New("kafkabp: Offset is invalid")
+)
