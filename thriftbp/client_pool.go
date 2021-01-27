reddit/baseplate.go

commit 8354a04363bf91b5f89e018e46993e4b882af831

Upgrade thrift
- Fix a bug in the last upgrade
- Make response headers available to clients using client pool

thriftbp/client_pool.go
@@ -455,7 +455,7 @@ type clientPool struct {
 	wrappedClient thrift.TClient
 }
 
-func (p *clientPool) Call(ctx context.Context, method string, args, result thrift.TStruct) (err error) {
+func (p *clientPool) Call(ctx context.Context, method string, args, result thrift.TStruct) (thrift.ResponseMeta, error) {
 	// A clientPool needs to be set up properly before it can be used,
 	// specifically use p.wrapCalls to set up p.wrappedClient before using it.
 	//
@@ -470,7 +470,7 @@ func (p *clientPool) Call(ctx context.Context, method string, args, result thrif
 // already takes care of this for you.
 func (p *clientPool) wrapCalls(middlewares ...thrift.ClientMiddleware) {
 	p.wrappedClient = thrift.WrapClient(thrift.WrappedTClient{
-		Wrapped: func(ctx context.Context, method string, args, result thrift.TStruct) error {
+		Wrapped: func(ctx context.Context, method string, args, result thrift.TStruct) (thrift.ResponseMeta, error) {
 			return p.pooledCall(ctx, method, args, result)
 		},
 	}, middlewares...)
@@ -481,10 +481,10 @@ func (p *clientPool) wrapCalls(middlewares ...thrift.ClientMiddleware) {
 //
 // This is not called directly, but is rather the inner "Call" wrapped by
 // wrapCalls, so it runs after all of the middleware.
-func (p *clientPool) pooledCall(ctx context.Context, method string, args, result thrift.TStruct) (err error) {
+func (p *clientPool) pooledCall(ctx context.Context, method string, args, result thrift.TStruct) (thrift.ResponseMeta, error) {
 	client, err := p.getClient()
 	if err != nil {
-		return PoolError{Cause: err}
+		return thrift.ResponseMeta{}, PoolError{Cause: err}
 	}
 	defer func() {
 		if shouldCloseConnection(err) {

WORKSPACE
@@ -5,11 +5,11 @@ load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 GO_VERSION = "1.15.7"
 
 # For rules_go
-RULES_GO_VERSION = "v0.25.0"
+RULES_GO_VERSION = "v0.25.1"
 
 http_archive(
     name = "io_bazel_rules_go",
-    sha256 = "6f111c57fd50baf5b8ee9d63024874dd2a014b069426156c55adbf6d3d22cb7b",
+    sha256 = "7904dbecbaffd068651916dce77ff3437679f9d20e1a7956bff43826e7645fcc",
     urls = [
         "https://mirror.bazel.build/github.com/bazelbuild/rules_go/releases/download/%s/rules_go-%s.tar.gz" % (RULES_GO_VERSION, RULES_GO_VERSION),
         "https://github.com/bazelbuild/rules_go/releases/download/%s/rules_go-%s.tar.gz" % (RULES_GO_VERSION, RULES_GO_VERSION),
@@ -17,11 +17,11 @@ http_archive(
 )
 
 # For gazelle
-GAZELLE_VERSION = "v0.22.2"
+GAZELLE_VERSION = "v0.22.3"
 
 http_archive(
     name = "bazel_gazelle",
-    sha256 = "b85f48fa105c4403326e9525ad2b2cc437babaa6e15a3fc0b1dbab0ab064bc7c",
+    sha256 = "222e49f034ca7a1d1231422cdb67066b885819885c356673cb1f72f748a3c9d4",
     urls = [
         "https://storage.googleapis.com/bazel-mirror/github.com/bazelbuild/bazel-gazelle/releases/download/%s/bazel-gazelle-%s.tar.gz" % (GAZELLE_VERSION, GAZELLE_VERSION),
         "https://github.com/bazelbuild/bazel-gazelle/releases/download/%s/bazel-gazelle-%s.tar.gz" % (GAZELLE_VERSION, GAZELLE_VERSION),

breakerbp/failure_ratio.go
@@ -108,11 +108,12 @@ func (cb FailureRatioBreaker) Execute(fn func() (interface{}, error)) (interface
 // ThriftMiddleware is a thrift.ClientMiddleware that handles circuit breaking.
 func (cb FailureRatioBreaker) ThriftMiddleware(next thrift.TClient) thrift.TClient {
 	return thrift.WrappedTClient{
-		Wrapped: func(ctx context.Context, method string, args, result thrift.TStruct) error {
-			_, err := cb.Execute(func() (interface{}, error) {
-				return nil, next.Call(ctx, method, args, result)
+		Wrapped: func(ctx context.Context, method string, args, result thrift.TStruct) (thrift.ResponseMeta, error) {
+			m, err := cb.Execute(func() (interface{}, error) {
+				return next.Call(ctx, method, args, result)
 			})
-			return err
+			meta, _ := m.(thrift.ResponseMeta)
+			return meta, err
 		},
 	}
 }

breakerbp/failure_ratio_test.go
@@ -75,7 +75,7 @@ func (config testConfig) run(t *testing.T) {
 	mockRequests(config, mock, client)
 	// reset client to succeed
 	mock.AddNopMockCalls(testMethod)
-	err := client.Call(context.Background(), testMethod, nil, nil)
+	_, err := client.Call(context.Background(), testMethod, nil, nil)
 	if err == nil && config.shouldFail {
 		t.Error("expected to fail, but call returned without error")
 	} else if err != nil && !config.shouldFail {
@@ -90,8 +90,8 @@ func mockRequests(config testConfig, mock *thrifttest.MockClient, client thrift.
 	}
 	mock.AddMockCall(
 		testMethod,
-		func(_ context.Context, args, result thrift.TStruct) error {
-			return errors.New("backend down")
+		func(_ context.Context, args, result thrift.TStruct) (thrift.ResponseMeta, error) {
+			return thrift.ResponseMeta{}, errors.New("backend down")
 		},
 	)
 	for i := 0; i < config.numFailures; i++ {

external.bzl
@@ -34,8 +34,8 @@ def go_dependencies():
     go_repository(
         name = "com_github_apache_thrift",
         importpath = "github.com/apache/thrift",
-        sum = "h1:kDzuYj7Rec8Oe5gItpFaeMGkV7SYAiHP3tT9/ayq01Q=",
-        version = "v0.13.1-0.20210120194924-d9fcdd3dbafb",
+        sum = "h1:J4dS9DgqdyU18qH50c/R+8LBmj/2mvyXAdfHyQ4qOuE=",
+        version = "v0.13.1-0.20210123045027-c2ddaf076649",
     )
     go_repository(
         name = "com_github_armon_consul_api",

go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/VividCortex/gohistogram v1.0.0 // indirect
 	github.com/alicebob/gopher-json v0.0.0-20180125190556-5a6b3ba71ee6 // indirect
 	github.com/alicebob/miniredis v2.5.0+incompatible
-	github.com/apache/thrift v0.13.1-0.20210120194924-d9fcdd3dbafb
+	github.com/apache/thrift v0.13.1-0.20210123045027-c2ddaf076649
 	github.com/avast/retry-go v2.6.1+incompatible
 	github.com/getsentry/sentry-go v0.6.0
 	github.com/go-kit/kit v0.9.0

go.sum
@@ -17,8 +17,8 @@ github.com/alicebob/gopher-json v0.0.0-20180125190556-5a6b3ba71ee6 h1:45bxf7AZMw
 github.com/alicebob/gopher-json v0.0.0-20180125190556-5a6b3ba71ee6/go.mod h1:SGnFV6hVsYE877CKEZ6tDNTjaSXYUk6QqoIK6PrAtcc=
 github.com/alicebob/miniredis v2.5.0+incompatible h1:yBHoLpsyjupjz3NL3MhKMVkR41j82Yjf3KFv7ApYzUI=
 github.com/alicebob/miniredis v2.5.0+incompatible/go.mod h1:8HZjEj4yU0dwhYHky+DxYx+6BMjkBbe5ONFIF1MXffk=
-github.com/apache/thrift v0.13.1-0.20210120194924-d9fcdd3dbafb h1:kDzuYj7Rec8Oe5gItpFaeMGkV7SYAiHP3tT9/ayq01Q=
-github.com/apache/thrift v0.13.1-0.20210120194924-d9fcdd3dbafb/go.mod h1:cp2SuWMxlEZw2r+iP2GNCdIi4C1qmUzdZFSVb+bacwQ=
+github.com/apache/thrift v0.13.1-0.20210123045027-c2ddaf076649 h1:J4dS9DgqdyU18qH50c/R+8LBmj/2mvyXAdfHyQ4qOuE=
+github.com/apache/thrift v0.13.1-0.20210123045027-c2ddaf076649/go.mod h1:cp2SuWMxlEZw2r+iP2GNCdIi4C1qmUzdZFSVb+bacwQ=
 github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6/go.mod h1:grANhF5doyWs3UAsr3K4I6qtAmlQcZDesFNEHPZAzj8=
 github.com/avast/retry-go v2.6.1+incompatible h1:quvLI98pOPWtTq7xnbX4TI5l9PmRJooM2AI1T7mOFUA=
 github.com/avast/retry-go v2.6.1+incompatible/go.mod h1:XtSnn+n/sHqQIpZ10K1qAevBhOOCWBLXXy3hyiqqBrY=

internal/gen-go/reddit/baseplate/baseplate.go
@@ -562,11 +562,11 @@ func (p *Error) String() string {
 }
 
 func (p *Error) Error() string {
-return p.String()
+  return p.String()
 }
 
 func (Error) TExceptionType() thrift.TExceptionType {
-return thrift.TExceptionTypeCompiled
+  return thrift.TExceptionTypeCompiled
 }
 
 var _ thrift.TException = (*Error)(nil)
@@ -599,6 +599,7 @@ type BaseplateService interface {  //The base for any baseplate-based service.
 //
 type BaseplateServiceClient struct {
   c thrift.TClient
+  meta thrift.ResponseMeta
 }
 
 func NewBaseplateServiceClientFactory(t thrift.TTransport, f thrift.TProtocolFactory) *BaseplateServiceClient {
@@ -622,6 +623,15 @@ func NewBaseplateServiceClient(c thrift.TClient) *BaseplateServiceClient {
 func (p *BaseplateServiceClient) Client_() thrift.TClient {
   return p.c
 }
+
+func (p *BaseplateServiceClient) LastResponseMeta_() thrift.ResponseMeta {
+  return p.meta
+}
+
+func (p *BaseplateServiceClient) SetLastResponseMeta_(meta thrift.ResponseMeta) {
+  p.meta = meta
+}
+
 // Return whether or not the service is healthy.
 // 
 // The healthchecker (baseplate.server.healthcheck) expects this endpoint to
@@ -633,7 +643,10 @@ func (p *BaseplateServiceClient) Client_() thrift.TClient {
 func (p *BaseplateServiceClient) IsHealthy(ctx context.Context) (r bool, err error) {
   var _args2 BaseplateServiceIsHealthyArgs
   var _result3 BaseplateServiceIsHealthyResult
-  if err = p.Client_().Call(ctx, "is_healthy", &_args2, &_result3); err != nil {
+  var meta thrift.ResponseMeta
+  meta, err = p.Client_().Call(ctx, "is_healthy", &_args2, &_result3)
+  p.SetLastResponseMeta_(meta)
+  if err != nil {
     return
   }
   return _result3.GetSuccess(), nil
@@ -942,6 +955,7 @@ type BaseplateServiceV2 interface {  //The base for any baseplate-based service.
 //
 type BaseplateServiceV2Client struct {
   c thrift.TClient
+  meta thrift.ResponseMeta
 }
 
 func NewBaseplateServiceV2ClientFactory(t thrift.TTransport, f thrift.TProtocolFactory) *BaseplateServiceV2Client {
@@ -965,6 +979,15 @@ func NewBaseplateServiceV2Client(c thrift.TClient) *BaseplateServiceV2Client {
 func (p *BaseplateServiceV2Client) Client_() thrift.TClient {
   return p.c
 }
+
+func (p *BaseplateServiceV2Client) LastResponseMeta_() thrift.ResponseMeta {
+  return p.meta
+}
+
+func (p *BaseplateServiceV2Client) SetLastResponseMeta_(meta thrift.ResponseMeta) {
+  p.meta = meta
+}
+
 // Return whether or not the service is healthy.
 // 
 // The healthchecker (baseplate.server.healthcheck) expects this endpoint to
@@ -980,7 +1003,10 @@ func (p *BaseplateServiceV2Client) IsHealthy(ctx context.Context, request *IsHea
   var _args6 BaseplateServiceV2IsHealthyArgs
   _args6.Request = request
   var _result7 BaseplateServiceV2IsHealthyResult
-  if err = p.Client_().Call(ctx, "is_healthy", &_args6, &_result7); err != nil {
+  var meta thrift.ResponseMeta
+  meta, err = p.Client_().Call(ctx, "is_healthy", &_args6, &_result7)
+  p.SetLastResponseMeta_(meta)
+  if err != nil {
     return
   }
   return _result7.GetSuccess(), nil

commit fa2a1efc3e3d5ad6d12ee419b9ef42ca389e2221

thriftbp: Fix client pool bug
This fixes a bug introduced in 8354a04 causing us to never close broken
connections and always return them back to the pool.

Also take advantage of the newly added TException types to help
determine whether to close the connection or return to the pool.

thriftbp/client_pool.go
@@ -481,8 +481,9 @@ func (p *clientPool) wrapCalls(middlewares ...thrift.ClientMiddleware) {
 //
 // This is not called directly, but is rather the inner "Call" wrapped by
 // wrapCalls, so it runs after all of the middleware.
-func (p *clientPool) pooledCall(ctx context.Context, method string, args, result thrift.TStruct) (thrift.ResponseMeta, error) {
-	client, err := p.getClient()
+func (p *clientPool) pooledCall(ctx context.Context, method string, args, result thrift.TStruct) (_ thrift.ResponseMeta, err error) {
+	var client Client
+	client, err = p.getClient()
 	if err != nil {
 		return thrift.ResponseMeta{}, PoolError{Cause: err}
 	}
@@ -520,6 +521,21 @@ func (p *clientPool) releaseClient(c Client) {
 func shouldCloseConnection(err error) bool {
 	if err == nil {
 		return false
+	}
+	var te thrift.TException
+	if errors.As(err, &te) {
+		switch te.TExceptionType() {
+		case thrift.TExceptionTypeApplication, thrift.TExceptionTypeProtocol, thrift.TExceptionTypeTransport:
+			// We definitely should close the connection on TTransportException.
+			// We probably don't need to close the connection on TApplicationException
+			// and TProtocolException, but just close them to be safe, as the
+			// connection might be in some weird state when those errors happen.
+			return true
+		case thrift.TExceptionTypeCompiled:
+			// For exceptions defined from the IDL, we definitely shouldn't close the
+			// connection.
+			return false
+		}
 	}
 	// We should avoid reusing the client if it hits a network error.
 	// We should also actively close the connection if it's a timeout,