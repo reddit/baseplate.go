04f62947e7cf8db9899c82778f4ef3d2cd5e5fad
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2021-04-09 09:14:57 -0700

    tracing: Add more doc and example to SpanTypeOption
    The new requirement from Baseplate spec regarding the span name of
    client spans are not well documented, so add some doc to emphasize it.
    Also add an example to it.

---
+++ tracing/start_options_example_test.go
@@ -0,0 +1,47 @@
+package tracing_test
+
+import (
+	"context"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/reddit/baseplate.go/tracing"
+)
+
+// This example demonstrates how to use SpanTypeOption to create a client span.
+func ExampleSpanTypeOption_client() {
+	var (
+		// In real code this should be in the args of your function
+		ctx context.Context
+		// In real code this should be the named return of your function
+		err error
+	)
+
+	span, ctx := opentracing.StartSpanFromContext(
+		ctx,
+		"service.endpoint", // For example, "mysql.query"
+		tracing.SpanTypeOption{
+			Type: tracing.SpanTypeClient,
+		},
+	)
+	// NOTE: It's important to wrap span.FinishWithOptions in a lambda.
+	//
+	// If you just do this:
+	//
+	//     // Bad example, DO NOT USE.
+	//     defer span.FinishWithOptions(tracing.FinishOptions{
+	//       Ctx: ctx,
+	//       Err: err,
+	//     }.Convert())
+	//
+	// Err will always be nil,
+	// because the args of defer'd function are evaluated at the time of defer,
+	// not time of execution.
+	defer func() {
+		span.FinishWithOptions(tracing.FinishOptions{
+			Ctx: ctx,
+			Err: err,
+		}.Convert())
+	}()
+
+	// Do real work here.
+}
