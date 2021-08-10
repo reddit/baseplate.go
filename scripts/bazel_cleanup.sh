2eb751a3e8fd74d7fb95a90e22cb763915e1cfc3
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2021-08-10 13:05:19 -0700

    Improve bazel script
    To work around https://github.com/bazelbuild/bazel-gazelle/issues/1088.

--- scripts/bazel_cleanup.sh
+++ scripts/bazel_cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Temporary solution to workaround https://github.com/bazelbuild/bazel-gazelle/issues/1088
+echo "def go_dependencies(): pass" > external.bzl
+
 bazel run //:gazelle -- update-repos -from_file=go.mod -prune -to_macro=external.bzl%go_dependencies
 bazel run //:gazelle -- fix
 
