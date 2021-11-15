5d4536c325122ba2bf457853741c5725ad4ce1ef
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2021-11-15 09:10:09 -0800

    Fix things shellcheck complained on shell scripts
    Currently the only shell script we have is linters.sh.

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-DIR=$(dirname $0)/..
-cd $DIR
+DIR=$(dirname "$0")/..
+cd "$DIR" || exit 1
 
-FILES=`find . -name "*.go" | grep -v -e "\/gen-go\/"`
+FILES=$(find . -name "*.go" | grep -v -e "\/gen-go\/")
 FAILED=0
 
 for FILE in $FILES; do
-  FMT=`gofmt -s -d $FILE`
+  FMT=$(gofmt -s -d "$FILE")
   if [ -n "$FMT" ]; then
     echo "gofmt:"
     echo "$FILE:"
@@ -16,16 +16,16 @@ for FILE in $FILES; do
   fi
 done
 
-VET=`go vet ./...`
+VET=$(go vet ./...)
 if [ -n "$VET" ]; then
   echo "go vet:"
   echo "$VET"
   FAILED=1
 fi
 
-STATICCHECK=`staticcheck ./...`
+STATICCHECK=$(staticcheck ./...)
 if [ -n "$STATICCHECK" ]; then
-  echo "`staticcheck --version`:"
+  echo "$(staticcheck --version):"
   echo "$STATICCHECK"
   FAILED=1
 fi
