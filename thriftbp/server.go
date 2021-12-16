bc086bdc11206042e0bde3e7ee5bbd3926a5194d
Marco Ferrer <35935108+marcoferrer@users.noreply.github.com>
2021-12-16 15:19:01 -0500

    prevent overwriting server logger config (#452)

--- thriftbp/server.go
+++ thriftbp/server.go
@@ -106,12 +106,14 @@ func NewBaseplateServer(
 	)
 	middlewares = append(middlewares, cfg.Middlewares...)
 	cfg.Middlewares = middlewares
-	cfg.Logger = log.ZapWrapper(log.ZapWrapperArgs{
-		Level: bp.GetConfig().Log.Level,
-		KVPairs: map[string]interface{}{
-			"from": "thrift",
-		},
-	}).ToThriftLogger()
+	if cfg.Logger == nil {
+		cfg.Logger = log.ZapWrapper(log.ZapWrapperArgs{
+			Level: bp.GetConfig().Log.Level,
+			KVPairs: map[string]interface{}{
+				"from": "thrift",
+			},
+		}).ToThriftLogger()
+	}
 	cfg.Addr = bp.GetConfig().Addr
 	cfg.Socket = nil
 	srv, err := NewServer(cfg)
