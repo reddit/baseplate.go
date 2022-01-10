f2f7cbaca3a30468b0e14e13f2c8dcca80cc6256
Jessica G <14338270+JessicaGreben@users.noreply.github.com>
2022-01-10 13:49:33 -0800

    Add http prometheus metrics for client and server (#451)
    add http client and server metrics for prometheus.

--- prometheusbp/promtest/testutil.go
+++ prometheusbp/promtest/testutil.go
@@ -5,15 +5,16 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/testutil"
+	dto "github.com/prometheus/client_model/go"
 )
 
 // PrometheusMetricTest stores information about a metric to use for testing.
 type PrometheusMetricTest struct {
-	tb          testing.TB
-	metric      prometheus.Collector
-	name        string
-	initValue   float64
-	labelValues []string
+	tb        testing.TB
+	metric    prometheus.Collector
+	name      string
+	initValue float64
+	labels    prometheus.Labels
 }
 
 // CheckDelta checks that the metric value changes exactly delta from when Helper was called.
@@ -35,12 +36,12 @@ func (p *PrometheusMetricTest) CheckExists() {
 
 // NewPrometheusMetricTest creates a new test object for a Prometheus metric.
 // It stores the current value of the metric along with the metric name.
-func NewPrometheusMetricTest(tb testing.TB, name string, metric prometheus.Collector, labelValues ...string) *PrometheusMetricTest {
+func NewPrometheusMetricTest(tb testing.TB, name string, metric prometheus.Collector, labels prometheus.Labels) *PrometheusMetricTest {
 	p := &PrometheusMetricTest{
-		tb:          tb,
-		metric:      metric,
-		name:        name,
-		labelValues: labelValues,
+		tb:     tb,
+		metric: metric,
+		name:   name,
+		labels: labels,
 	}
 	p.initValue = p.getValue()
 	return p
@@ -51,21 +52,62 @@ func (p *PrometheusMetricTest) getValue() float64 {
 	var value float64
 	switch m := p.metric.(type) {
 	case *prometheus.GaugeVec:
-		gague, err := m.GetMetricWithLabelValues(p.labelValues...)
+		gague, err := m.GetMetricWith(p.labels)
 		if err != nil {
 			p.tb.Fatalf("get %s metric err %v", p.name, err)
 		}
 		value = testutil.ToFloat64(gague)
 	case *prometheus.CounterVec:
-		counter, err := m.GetMetricWithLabelValues(p.labelValues...)
+		counter, err := m.GetMetricWith(p.labels)
 		if err != nil {
 			p.tb.Fatalf("get %s metric err %v", p.name, err)
 		}
 		value = testutil.ToFloat64(counter)
 	case *prometheus.HistogramVec:
-		// prometheus.HistogramVec not supported for testutil.ToFloat64
+		histrogram, err := m.GetMetricWith(p.labels)
+		if err != nil {
+			p.tb.Fatalf("get %s metric err %v", p.name, err)
+		}
+		h, ok := histrogram.(prometheus.Collector)
+		if !ok {
+			p.tb.Fatalf("histogram is not a collector type")
+		}
+		_, value = collectHistogramToFloat64(p.tb, h)
 	default:
 		p.tb.Fatalf("not supported type %T\n", m)
 	}
 	return value
+}
+
+// collectHistogramToFloat64 returns the sample count and sum of the histogram.
+func collectHistogramToFloat64(tb testing.TB, c prometheus.Collector) (int, float64) {
+	var (
+		m      prometheus.Metric
+		mCount int
+		mChan  = make(chan prometheus.Metric)
+		done   = make(chan struct{})
+	)
+
+	go func() {
+		for m = range mChan {
+			mCount++
+		}
+		close(done)
+	}()
+
+	c.Collect(mChan)
+	close(mChan)
+	<-done
+
+	if mCount != 1 {
+		tb.Errorf("collected %d metrics instead of exactly 1", mCount)
+	}
+
+	pb := &dto.Metric{}
+	m.Write(pb)
+	if pb.Histogram != nil {
+		return int(pb.GetHistogram().GetSampleCount()), pb.GetHistogram().GetSampleSum()
+	}
+	tb.Errorf("collected a non-histogram type metric: %s", pb)
+	return 0, 0
 }
