94e25e89ad3975850de7994fd150f20a350afa28
Zack Keim <zack.keim@reddit.com>
2022-02-28 15:19:42 -0800

    Revise redisxtest document

--- redis/cache/redisx/redisxtest/doc.go
+++ redis/cache/redisx/redisxtest/doc.go
@@ -1,2 +1,4 @@
-// Package redisxtest provides utilities for baseplate.go/redids/cache/redisx testing
+// Package redisxtest provides utilities for baseplate.go/redids/cache/redisx testing.
+//
+// This includes functions for mocking redis instances.
 package redisxtest
