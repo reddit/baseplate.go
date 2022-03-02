a07bb694d17c84b32cc26b851203a7aef0784bc5
Zack Keim <zack.keim@reddit.com>
2022-03-02 14:01:40 -0800

    Add MockRedisCluster wrapper

--- redis/cache/redisx/redisxtest/redisxtest.go
+++ redis/cache/redisx/redisxtest/redisxtest.go
@@ -11,20 +11,46 @@ import (
 	"github.com/reddit/baseplate.go/redis/cache/redisx"
 )
 
-// NewMockRedisClient sets up a mock redis cluster, client and sender
-// This should be called from TestMain since the miniredis instance created is locked
-func NewMockRedisClient(ctx context.Context, timeout time.Duration) (client redisx.Syncx, teardown func(), err error) {
+type MockRedisCluster struct {
+	redisCluster *miniredis.Miniredis
+	teardown     func()
+}
+
+func NewMockRedisCluster() (MockRedisCluster, error) {
 	redisCluster, err := miniredis.Run()
 	if err != nil {
-		return redisx.Syncx{}, nil, err
+		return MockRedisCluster{}, err
 	}
 
+	teardown := func() {
+		redisCluster.Close()
+	}
+
+	return MockRedisCluster{
+		redisCluster: redisCluster,
+		teardown:     teardown,
+	}, nil
+}
+
+func (mrc *MockRedisCluster) Addr() string {
+	return mrc.redisCluster.Addr()
+}
+
+func (mrc *MockRedisCluster) Close() {
+	mrc.redisCluster.Close()
+}
+
+// NewMockRedisClient sets up a client and sender to a mock redis cluster
+func NewMockRedisClient(ctx context.Context, redisCluster MockRedisCluster, timeout time.Duration) (client redisx.Syncx, teardown func(), err error) {
+
+	// Create connection
 	conn, err := redisconn.Connect(ctx, redisCluster.Addr(), redisconn.Opts{IOTimeout: timeout})
 	if err != nil {
 		redisCluster.Close()
 		return redisx.Syncx{}, nil, err
 	}
 
+	// Create client
 	client = redisx.Syncx{
 		Sync: redisx.BaseSync{SyncCtx: redis.SyncCtx{S: conn}},
 	}
