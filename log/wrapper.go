25d3006a457c2af8a72ed7686345b884b171a9ef
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2022-04-04 16:41:59 -0700

    log: Add tb.Helper() to TestWrapper
    The filewatcher test becomes extremely flaky on Github Actions recently,
    and without tb.Helper() call the line number is not useful at all.

--- log/wrapper.go
+++ log/wrapper.go
@@ -206,6 +206,7 @@ func StdWrapper(logger *stdlog.Logger) Wrapper {
 // It fails the test when called.
 func TestWrapper(tb testing.TB) Wrapper {
 	return func(_ context.Context, msg string) {
+		tb.Helper()
 		tb.Errorf("logger called with msg: %q", msg)
 	}
 }
