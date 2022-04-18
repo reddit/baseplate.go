489703d4aa6f86927a5deb513a53844eaf758a61
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2022-04-18 11:41:01 -0700

    redispipebp: Add prom timer for MonitoredClient

--- redis/cache/redispipebp/monitored.go
+++ redis/cache/redispipebp/monitored.go
@@ -3,19 +3,52 @@ package redispipebp
 import (
 	"context"
 	"errors"
+	"strings"
+	"time"
 
 	"github.com/joomcode/errorx"
 	"github.com/joomcode/redispipe/redis"
 	"github.com/opentracing/opentracing-go"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 
+	"github.com/reddit/baseplate.go/prometheusbp"
 	"github.com/reddit/baseplate.go/redis/cache/redisx"
 	"github.com/reddit/baseplate.go/tracing"
+)
+
+const (
+	promNamespace = "redispipebp"
+
+	labelCommand = "redis_command"
+	labelSuccess = "redis_success"
+)
+
+var (
+	labels = []string{
+		labelCommand,
+		labelSuccess,
+	}
+
+	promHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: promNamespace,
+		Name:      "latency_seconds",
+		Help:      "Redis latency in seconds",
+		Buckets:   prometheusbp.DefaultBuckets,
+	}, labels)
 )
 
 // MonitoredSync wraps Sync methods in client spans.
 type MonitoredSync struct {
 	Sync redisx.Sync
 	Name string
+}
+
+func extractCommand(cmd string) string {
+	if index := strings.IndexByte(cmd, ' '); index >= 0 {
+		return cmd[:index]
+	}
+	return cmd
 }
 
 // Do wraps s.Sync.Do in a client span.
@@ -25,12 +58,17 @@ func (s MonitoredSync) Do(ctx context.Context, cmd string, args ...interface{}) 
 		s.Name+".do",
 		tracing.SpanTypeOption{Type: tracing.SpanTypeClient},
 	)
-	defer func() {
+	defer func(start time.Time) {
+		err := redis.AsError(result)
+		promHistogram.With(prometheus.Labels{
+			labelCommand: extractCommand(cmd),
+			labelSuccess: prometheusbp.BoolString(err == nil),
+		}).Observe(time.Since(start).Seconds())
 		span.FinishWithOptions(tracing.FinishOptions{
 			Ctx: ctx,
-			Err: redis.AsError(result),
+			Err: err,
 		}.Convert())
-	}()
+	}(time.Now())
 
 	return s.Sync.Do(ctx, cmd, args...)
 }
@@ -42,24 +80,34 @@ func (s MonitoredSync) Send(ctx context.Context, r redis.Request) (result interf
 		s.Name+".send",
 		tracing.SpanTypeOption{Type: tracing.SpanTypeClient},
 	)
-	defer func() {
+	defer func(start time.Time) {
+		err := redis.AsError(result)
+		cmd := extractCommand(r.Cmd)
+		if cmd == "" {
+			cmd = "send"
+		}
+		promHistogram.With(prometheus.Labels{
+			labelCommand: cmd,
+			labelSuccess: prometheusbp.BoolString(err == nil),
+		}).Observe(time.Since(start).Seconds())
 		span.FinishWithOptions(tracing.FinishOptions{
 			Ctx: ctx,
 			Err: redis.AsError(result),
 		}.Convert())
-	}()
+	}(time.Now())
 
 	return s.Sync.Send(ctx, r)
 }
 
 // SendMany wraps s.Sync.SendMany in a client span.
 func (s MonitoredSync) SendMany(ctx context.Context, reqs []redis.Request) (results []interface{}) {
+	const cmd = "send-many"
 	span, ctx := opentracing.StartSpanFromContext(
 		ctx,
-		s.Name+".send-many",
+		s.Name+"."+cmd,
 		tracing.SpanTypeOption{Type: tracing.SpanTypeClient},
 	)
-	defer func() {
+	defer func(start time.Time) {
 		var err error
 		if len(results) > 0 {
 			first := redis.AsError(results[0])
@@ -80,28 +128,37 @@ func (s MonitoredSync) SendMany(ctx context.Context, reqs []redis.Request) (resu
 				}
 			}
 		}
+		promHistogram.With(prometheus.Labels{
+			labelCommand: cmd,
+			labelSuccess: prometheusbp.BoolString(err == nil),
+		}).Observe(time.Since(start).Seconds())
 		span.FinishWithOptions(tracing.FinishOptions{
 			Ctx: ctx,
 			Err: err,
 		}.Convert())
-	}()
+	}(time.Now())
 
 	return s.Sync.SendMany(ctx, reqs)
 }
 
 // SendTransaction wraps s.Sync.SendTransaction in a client span.
 func (s MonitoredSync) SendTransaction(ctx context.Context, reqs []redis.Request) (results []interface{}, err error) {
+	const cmd = "send-transaction"
 	span, ctx := opentracing.StartSpanFromContext(
 		ctx,
-		s.Name+".send-transaction",
+		s.Name+"."+cmd,
 		tracing.SpanTypeOption{Type: tracing.SpanTypeClient},
 	)
-	defer func() {
+	defer func(start time.Time) {
+		promHistogram.With(prometheus.Labels{
+			labelCommand: cmd,
+			labelSuccess: prometheusbp.BoolString(err == nil),
+		}).Observe(time.Since(start).Seconds())
 		span.FinishWithOptions(tracing.FinishOptions{
 			Ctx: ctx,
 			Err: err,
 		}.Convert())
-	}()
+	}(time.Now())
 
 	return s.Sync.SendTransaction(ctx, reqs)
 }
@@ -130,12 +187,17 @@ func (s MonitoredScanIterator) Next() (results []string, err error) {
 		s.name+".next",
 		tracing.SpanTypeOption{Type: tracing.SpanTypeClient},
 	)
-	defer func() {
+	defer func(start time.Time) {
+		const cmd = "scanner-next"
+		promHistogram.With(prometheus.Labels{
+			labelCommand: cmd,
+			labelSuccess: prometheusbp.BoolString(err == nil),
+		}).Observe(time.Since(start).Seconds())
 		span.FinishWithOptions(tracing.FinishOptions{
 			Ctx: ctx,
 			Err: err,
 		}.Convert())
-	}()
+	}(time.Now())
 	return s.ScanIterator.Next()
 }
 
