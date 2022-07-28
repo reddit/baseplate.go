a79b3115538ba5436cc04b7d8bda1d0d17e743cd
Matthew Terwilliger <matt.terwilliger@reddit.com>
2022-07-28 10:45:57 -0400

    maxprocs: Initial implementation

---
+++ runtimebp/internal/maxprocs/maxprocs.go
@@ -0,0 +1,111 @@
+// Package maxprocs optimizes GOMAXPROCS for Infrared environments.
+// It attempts to balance application performance with cluster-level resource utilization.
+// Applications may tune GOMAXPROCS as necessary (see Set).
+// The defaults in this package are subject to change.
+package maxprocs
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"runtime"
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+type floatEnv struct {
+	key   string
+	gauge *prometheus.GaugeVec
+
+	present bool
+	raw     string
+	val     float64
+}
+
+const (
+	// The default scale applies when request is set but not scale (or scale is
+	// invalid). It defaults to some "oversubscription" to take advantage of
+	// excess Kubernetes node capacity when available.
+	//
+	// Extremely performance-sensitive services should set scale to 1.0 to
+	// ensure consistent performance.
+	//
+	// This value was NOT chosen scientifically and IS subject to change.
+	defaultCPURequestScale = 1.5
+
+	setByGOMAXPROCS = "gomaxprocs"
+	setByRequest    = "cpu_request"
+)
+
+var (
+	mEnvGOMAXPROCS      = promauto.NewGaugeVec(prometheus.GaugeOpts{Name: "baseplate_go_env_gomaxprocs"}, []string{"present"})
+	mEnvCPURequest      = promauto.NewGaugeVec(prometheus.GaugeOpts{Name: "baseplate_go_env_baseplate_cpu_request"}, []string{"present"})
+	mEnvCPURequestScale = promauto.NewGaugeVec(prometheus.GaugeOpts{Name: "baseplate_go_env_baseplate_cpu_request_scale"}, []string{"present"})
+
+	initialGOMAXPROCS = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "baseplate_go_initial_gomaxprocs",
+	}, []string{"set_by"})
+
+	_ = promauto.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "baseplate_go_current_gomaxprocs",
+	}, currentGOMAXPROCS)
+)
+
+// Set configures the runtime's GOMAXPROCS using the following heuristic:
+//   1. If $GOMAXPROCS is set, Set relinquishes control to the Go runtime.
+//      This should cause the runtime to respect this value directly.
+//   2. If $BASEPLATE_CPU_REQUEST is unset/invalid, Set relinquishes control to the Go runtime.
+//      This should cause the runtime to use the detected number of CPUs on this machine.
+//   3. Otherwise, $BASEPLATE_CPU_REQUEST is multiplied by $BASEPLATE_CPU_REQUEST_SCALE
+//      (or defaultCPURequestScale) to compute the new GOMAXPROCS, minimum 2.
+//
+// Set also exports several metrics to facilitate further tuning/analysis.
+func Set() {
+	envGOMAXPROCS := &floatEnv{key: "GOMAXPROCS", gauge: mEnvGOMAXPROCS}
+	envCPURequest := &floatEnv{key: "BASEPLATE_CPU_REQUEST", gauge: mEnvCPURequest}
+	envCPURequestScale := &floatEnv{key: "BASEPLATE_CPU_REQUEST_SCALE", gauge: mEnvCPURequestScale}
+
+	for _, env := range []*floatEnv{envGOMAXPROCS, envCPURequest, envCPURequestScale} {
+		env.raw, env.present = os.LookupEnv(env.key)
+		env.val, _ = strconv.ParseFloat(env.raw, 64)
+		env.gauge.WithLabelValues(strconv.FormatBool(env.present)).Set(env.val)
+	}
+
+	setBy := setByGOMAXPROCS
+	defer func() { initialGOMAXPROCS.WithLabelValues(setBy).Set(currentGOMAXPROCS()) }()
+
+	if envGOMAXPROCS.present {
+		return // let Go runtime handle it
+	}
+
+	if !envCPURequest.present {
+		return // let Go runtime handle it
+	}
+
+	if envCPURequest.val <= 0 {
+		// This should always be valid positive float in infrared-deployed applications.
+		fmt.Fprintf(os.Stderr, "maxprocs: $BASEPLATE_CPU_REQUEST=%q, want positive float. Falling back to Go's default", envCPURequest.raw)
+		return
+	}
+
+	scale := defaultCPURequestScale
+	if envCPURequestScale.val > 0 {
+		scale = envCPURequestScale.val
+	} else if envCPURequestScale.present {
+		fmt.Fprintf(os.Stderr, "maxprocs: $BASEPLATE_CPU_REQUEST_SCALE=%q, want positive float. Falling back to default of %g", envCPURequestScale.raw, scale)
+	}
+
+	setBy = setByRequest
+	runtime.GOMAXPROCS(int(
+		math.Max(
+			2, // to ensure some minimal parallelism
+			math.Ceil(envCPURequest.val*scale),
+		),
+	))
+}
+
+func currentGOMAXPROCS() float64 {
+	return float64(runtime.GOMAXPROCS(0))
+}
---
+++ runtimebp/internal/maxprocs/maxprocs_test.go
@@ -0,0 +1,115 @@
+package maxprocs
+
+import (
+	"context"
+	"runtime"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func TestSet(t *testing.T) {
+	sentinel := 99
+
+	for _, tt := range []struct {
+		name           string
+		env            map[string]string
+		wantGOMAXPROCS int
+	}{
+		{
+			name: "GOMAXPROCS",
+			env: map[string]string{
+				"GOMAXPROCS": "42",
+			},
+			wantGOMAXPROCS: sentinel, // since our package abdicates responsibility to Go
+		},
+		{
+			name: "request_no_scale",
+			env: map[string]string{
+				"BASEPLATE_CPU_REQUEST": "42",
+			},
+			wantGOMAXPROCS: 63, // 42 * 1.5 scale default multiplier
+		},
+		{
+			name: "request_with_scale",
+			env: map[string]string{
+				"BASEPLATE_CPU_REQUEST":       "42",
+				"BASEPLATE_CPU_REQUEST_SCALE": "0.9",
+			},
+			wantGOMAXPROCS: 38, // ceil(42 * 0.9)
+		},
+		{
+			name: "invalid_request",
+			env: map[string]string{
+				"BASEPLATE_CPU_REQUEST": "not a number",
+			},
+			wantGOMAXPROCS: sentinel,
+		},
+		{
+			name: "invalid_scale",
+			env: map[string]string{
+				"BASEPLATE_CPU_REQUEST":       "42",
+				"BASEPLATE_CPU_REQUEST_SCALE": "not a number",
+			},
+			wantGOMAXPROCS: 63, // 42 * 1.5 scale default multiplier
+		},
+		{
+			name: "zero_request",
+			env: map[string]string{
+				"BASEPLATE_CPU_REQUEST": "0",
+			},
+			wantGOMAXPROCS: sentinel,
+		},
+		{
+			name: "zero_scale",
+			env: map[string]string{
+				"BASEPLATE_CPU_REQUEST":       "42",
+				"BASEPLATE_CPU_REQUEST_SCALE": "0",
+			},
+			wantGOMAXPROCS: 63, // 42 * 1.5 scale default multiplier
+		},
+		{
+			name: "min_2",
+			env: map[string]string{
+				"BASEPLATE_CPU_REQUEST":       "1",
+				"BASEPLATE_CPU_REQUEST_SCALE": "1",
+			},
+			wantGOMAXPROCS: 2,
+		},
+		{
+			name: "gomaxprocs_and_request",
+			env: map[string]string{
+				"GOMAXPROCS":            "anything at all",
+				"BASEPLATE_CPU_REQUEST": "12",
+			},
+			wantGOMAXPROCS: sentinel, // since our package abdicates responsibility to Go
+		},
+		{
+			name:           "nothing_set",
+			env:            map[string]string{},
+			wantGOMAXPROCS: sentinel, // since our package abdicates responsibility to Go
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			orig := runtime.GOMAXPROCS(0)
+			defer runtime.GOMAXPROCS(orig)
+			// set GOMAXPROCS to a known value
+			runtime.GOMAXPROCS(sentinel)
+
+			// clear out metrics between runs since we care about the zero value
+			for _, m := range []*prometheus.GaugeVec{mEnvGOMAXPROCS, mEnvCPURequest, mEnvCPURequestScale, initialGOMAXPROCS} {
+				m.Reset()
+			}
+
+			for k, v := range tt.env {
+				t.Setenv(k, v)
+			}
+
+			Set(context.Background())
+
+			if got, want := runtime.GOMAXPROCS(0), tt.wantGOMAXPROCS; got != want {
+				t.Errorf("got GOMAXPROCS=%d, want %d", got, want)
+			}
+		})
+	}
+}
