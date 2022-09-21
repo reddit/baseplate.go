6d2f41b574b2fdc9069cc6041e8667812e34aeef
Gavriel Hirsch <gavriel.hirsch@reddit.com>
2022-09-21 11:41:47 -0700

    don't use build tags

--- prometheusbp/metrics.go
+++ prometheusbp/metrics.go
@@ -1,5 +1,32 @@
 package prometheusbp
 
+var (
+	// DefaultLatencyBuckets is the default bucket values for a prometheus histogram metric.
+	// These match the spec defined at https://github.snooguts.net/reddit/baseplate.spec/blob/master/component-apis/prom-metrics.md#latency-histograms
+	DefaultLatencyBuckets = []float64{
+		0.000_100,  // 100us
+		0.000_500,  // 500us
+		0.001_000,  // 1ms
+		0.002_500,  // 2.5ms
+		0.005_000,  // 5ms
+		0.010_000,  // 10ms
+		0.025_000,  // 25ms
+		0.050_000,  // 50ms
+		0.100_000,  // 100ms
+		0.250_000,  // 250ms
+		0.500_000,  // 500ms
+		1.000_000,  // 1s
+		5.000_000,  // 5s
+		15.000_000, // 15s
+		30.000_000, // 30s
+	}
+	// KafkaLatencyBuckets is the set of bucket values for Kafka-related prometheus histogram metrics.
+	// These match the spec defined at https://github.snooguts.net/reddit/baseplate.spec/blob/master/component-apis/prom-metrics.md#latency-histograms
+	KafkaLatencyBuckets = DefaultLatencyBuckets
+	// Deprecated: Please use DefaultLatencyBuckets instead for latencies, and define your own buckets for non-latency histograms
+	DefaultBuckets = DefaultLatencyBuckets
+)
+
 // BoolString returns the string version of a boolean value that should be used
 // in a prometheus metric label.
 func BoolString(b bool) string {
