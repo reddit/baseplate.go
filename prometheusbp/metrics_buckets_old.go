e453cdc8c4fa4bf5304463c7a0e3188c95ef4ab4
Gavriel Hirsch <gavriel.hirsch@reddit.com>
2022-09-21 11:29:56 -0700

    style fix, change var name

--- prometheusbp/metrics_buckets_old.go
+++ prometheusbp/metrics_buckets_old.go
@@ -7,8 +7,11 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-// DefaultBuckets is the default bucket values for a prometheus histogram metric.
-var DefaultBuckets = prometheus.ExponentialBuckets(0.0001, 2.5, 14) // 100us ~ 14.9s
-
-// KafkaBuckets is the set of bucket values for Kafka-related prometheus histogram metrics.
-var KafkaBuckets = prometheus.ExponentialBucketsRange(1e-4, 10, 10) // 100us - 10s
+var (
+	// DefaultLatencyBuckets is the default bucket values for a prometheus histogram metric.
+	DefaultLatencyBuckets = prometheus.ExponentialBuckets(0.0001, 2.5, 14) // 100us ~ 14.9s
+	// KafkaLatencyBuckets is the set of bucket values for Kafka-related prometheus histogram metrics.
+	KafkaLatencyBuckets = prometheus.ExponentialBucketsRange(1e-4, 10, 10) // 100us - 10s
+	// Deprecated: Please use DefaultLatencyBuckets instead for latencies, and define your own buckets for non-latency histograms
+	DefaultBuckets = DefaultLatencyBuckets
+)
