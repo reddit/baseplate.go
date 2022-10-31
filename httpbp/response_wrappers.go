574ce14d341f196870d858269e9078e82cc84ec2
Adam Sax <adam.sax@reddit.com>
2022-10-31 11:53:21 -0400

    remove generated code and write all wrappers by hand

--- httpbp/response_wrappers.go
+++ httpbp/response_wrappers.go
@@ -1,32 +1,75 @@
-// DO NOT EDIT.
-// This code was partially generated and then made human readable.
-// This approach was adapted from https://github.com/badgerodon/contextaware/blob/4c442dfd39106512496bdd13c42c451da8ddeff3/internal/generate-wrap/main.go
-// See also https://www.doxsey.net/blog/fixing-interface-erasure-in-go/ for more context
-
 package httpbp
 
 import (
 	"net/http"
 )
 
+type optionalResponseWriter uint64
+
+const (
+	flusher optionalResponseWriter = 1 << iota
+	hijacker
+	pusher
+)
+
 func wrapResponseWriter(orig, wrapped http.ResponseWriter) http.ResponseWriter {
-	var f uint64
-	flusher, isFlusher := orig.(http.Flusher)
-	if isFlusher { f |= 0x0001 }
-	hijacker, isHijacker := orig.(http.Hijacker)
-	if isHijacker { f |= 0x0002 }
-	pusher, isPusher := orig.(http.Pusher)
-	if isPusher { f |= 0x0004 }
+	var f optionalResponseWriter
+	fl, isFlusher := orig.(http.Flusher)
+	if isFlusher {
+		f |= flusher
+	}
+	h, isHijacker := orig.(http.Hijacker)
+	if isHijacker {
+		f |= hijacker
+	}
+	p, isPusher := orig.(http.Pusher)
+	if isPusher {
+		f |= pusher
+	}
 
 	switch f {
-	case 0x0000: return wrapped
-	case 0x0001: return struct{http.ResponseWriter;http.Flusher}{wrapped, flusher}
-	case 0x0002: return struct{http.ResponseWriter;http.Hijacker}{wrapped, hijacker}
-	case 0x0003: return struct{http.ResponseWriter;http.Flusher;http.Hijacker}{wrapped, flusher,hijacker}
-	case 0x0004: return struct{http.ResponseWriter;http.Pusher}{wrapped, pusher}
-	case 0x0005: return struct{http.ResponseWriter;http.Flusher;http.Pusher}{wrapped,flusher,pusher}
-	case 0x0006: return struct{http.ResponseWriter;http.Hijacker;http.Pusher}{wrapped, hijacker,pusher}
-	case 0x0007: return struct{http.ResponseWriter;http.Flusher;http.Hijacker;http.Pusher}{wrapped, flusher,hijacker,pusher}
+	case 0:
+		return wrapped
+	case flusher:
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+		}{wrapped, fl}
+	case hijacker:
+		return struct {
+			http.ResponseWriter
+			http.Hijacker
+		}{wrapped, h}
+	case flusher | hijacker:
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Hijacker
+		}{wrapped, fl, h}
+	case pusher:
+		return struct {
+			http.ResponseWriter
+			http.Pusher
+		}{wrapped, p}
+	case flusher | pusher:
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Pusher
+		}{wrapped, fl, p}
+	case hijacker | pusher:
+		return struct {
+			http.ResponseWriter
+			http.Hijacker
+			http.Pusher
+		}{wrapped, h, p}
+	case flusher | hijacker | pusher:
+		return struct {
+			http.ResponseWriter
+			http.Flusher
+			http.Hijacker
+			http.Pusher
+		}{wrapped, fl, h, p}
 	}
 
 	return wrapped
