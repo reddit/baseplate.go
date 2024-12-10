291b7bc040844f28bb9b4aed92ebad2b641bbc61
Hiram Silvey <9485196+HiramSilvey@users.noreply.github.com>
2024-12-10 15:22:21 -0500

    Fix style.

--- httpbp/client_middlewares.go
+++ httpbp/client_middlewares.go
@@ -17,7 +17,6 @@ import (
 
 	"github.com/reddit/baseplate.go/breakerbp"
 	"github.com/reddit/baseplate.go/internal/faults"
-
 	//lint:ignore SA1019 This library is internal only, not actually deprecated
 	"github.com/reddit/baseplate.go/internalv2compat"
 	"github.com/reddit/baseplate.go/retrybp"
