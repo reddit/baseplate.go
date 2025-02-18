233470f67e28347623f5c102d6cc58e2e4748dee
Andrew Boyle <andrew.boyle@reddit.com>
2025-02-18 16:32:28 -0600

    remove the thrift headerbp client middleware from defaults

--- thriftbp/client_middlewares.go
+++ thriftbp/client_middlewares.go
@@ -175,7 +175,6 @@ func BaseplateDefaultClientMiddlewares(args DefaultClientMiddlewareArgs) []thrif
 		BaseplateErrorWrapper,
 		thrift.ExtractIDLExceptionClientMiddleware,
 		SetDeadlineBudget,
-		ClientBaseplateHeadersMiddleware(args.ServiceSlug, args.ClientName),
 		clientFaultMiddleware.Middleware(), // clientFaultMiddleware MUST be last
 	)
 	return middlewares
