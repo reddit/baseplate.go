9a31bc2c678564b0cdc3ec69215094963d169bff
Ghazale Behboud <ghazale.behboud@reddit.com>
2025-09-15 10:58:28 -0700

    Deprecate events package in favor of GRPC Event Collector

--- events/doc.go
+++ events/doc.go
@@ -1,5 +1,8 @@
 // Package events implements event publisher per baseplate spec.
 //
+// Deprecated: This package is in maintenance mode and should not be used for new projects.
+// Consider using alternative event publishing solutions for new development.
+//
 // This package is mainly just the serialization part.
 // The actual publishing part is handled by sidecar implemented by baseplate.py,
 // and communicated via mqsend package.
