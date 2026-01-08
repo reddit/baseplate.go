c4950fb5e44cecb445d6c1840a02bfc2195bbb77
Daniel Nephin <dnephin@gmail.com>
2026-01-08 16:44:15 -0500

    Update thriftbp/client_pool.go
    Co-authored-by: Dan Couture <150738210+mathyourlife-reddit@users.noreply.github.com>

--- thriftbp/client_pool.go
+++ thriftbp/client_pool.go
@@ -598,7 +598,7 @@ func newClient(
 			TTransport: raw,
 		}
 		if err := transport.Open(); err != nil {
-			return nil, nil, fmt.Errorf("thriftbp: error opening TSocket to %v for new Thrift client: %w", addr, err)
+			return nil, nil, fmt.Errorf("thriftbp: error opening TSocket to %q for new Thrift client: %w", addr, err)
 		}
 
 		return thrift.NewTStandardClient(
